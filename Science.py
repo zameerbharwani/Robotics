cee597a1da83dcc20edb39756f339a0894258a68
Zameer Bharwani <zameerbharwani@hotmail.com>
2020-09-01 17:03:17 -0700

    Update Science.py

--- Science.py
+++ Science.py
@@ -10,64 +10,64 @@ class ScienceController:
 	
 	def __init__(self):
 
-		self.init_Auger=rospy.init_node("ScienceController", anonymous=True)
-		self.sub_switches=rospy.Subscriber("/science_limit_switches",UInt8,self.callback_switches) 
-		self.sub_xbox=rospy.Subscriber("/joy", Joy, self.callback_xbox)
-		self.pub=rospy.Publisher("/science", Frame, queue_size=20)
-		self.upper_switch=0
-		self.lower_switch=0
-		self.speed_dir=0 
-		self.speed_dir_spin=0
-		self.position=0
+		self.init_Auger = rospy.init_node("ScienceController", anonymous=True)
+		self.sub_switches = rospy.Subscriber("/science_limit_switches",UInt8,self.callback_switches) 
+		self.sub_xbox = rospy.Subscriber("/joy", Joy, self.callback_xbox)
+		self.pub = rospy.Publisher("/science", Frame, queue_size=20)
+		self.upper_switch = 0
+		self.lower_switch = 0 
+		self.speed_dir = 0 
+		self.speed_dir_spin = 0
+		self.position = 0
 		self.ID = 600 
-		self.ID_2=2000
+		self.ID_2 = 2000
 
 
 	def callback_switches(self,switches):
 
-		if (switches.data)&0x1==1: #check to see if the upper switch is being pressed
-			self.upper_switch=1
+		if (switches.data)&0x1 == 1: #check to see if the upper switch is being pressed
+			self.upper_switch = 1
 
 		else:
-			self.upper_switch=0
+			self.upper_switch = 0
 
 
-		if (switches.data>>1)&0x1==1: #check to see if the lower switch is being pressed
-			self.lower_switch=1
+		if (switches.data>>1)&0x1 == 1: #check to see if the lower switch is being pressed
+			self.lower_switch = 1
 
 		else:
 
-			self.lower_switch=0
+			self.lower_switch = 0
 		
 	def callback_xbox(self,joy):
 
-		self.speed_dir_spin=joy.axes[4] 
+		self.speed_dir_spin = joy.axes[4] 
 
-		if self.upper_switch==1: 		#upper limit switch pressed
+		if self.upper_switch == 1: 		#upper limit switch pressed
 
-			if joy.axes[1]<=0: 		#allowing you to move down, but not up
-				self.speed_dir=joy.axes[1]
+			if joy.axes[1] <= 0: 		#allowing you to move down, but not up
+				self.speed_dir = joy.axes[1]
 
 			else:
-				self.speed_dir=0 #if still trying to move up do nothing 
+				self.speed_dir = 0 #if still trying to move up do nothing 
 		
-		elif self.lower_switch==1: 		#lower limit switch pressed
+		elif self.lower_switch == 1: 		#lower limit switch pressed
 
-			if joy.axes[1]>=0:		#allowing you to move up, but not down
-				self.speed_dir=joy.axes[1]
+			if joy.axes[1] >= 0:		#allowing you to move up, but not down
+				self.speed_dir = joy.axes[1]
 
 			else:
-				self.speed_dir=0 			#if still trying to move down do nothing 
+				self.speed_dir = 0 			#if still trying to move down do nothing 
 
 		else:
-			self.speed_dir=joy.axes[1] 		#if neither sensor is pressed, movement in either direction is allowed
+			self.speed_dir = joy.axes[1] 		#if neither sensor is pressed, movement in either direction is allowed
 
 
-		if joy.buttons[2]==1: #A button 
-			self.position=1 #open
+		if joy.buttons[2] == 1: #A button 
+			self.position = 1 #open
 
-		elif joy.buttons[1]==1: #B button
-			self.position=2 #close 
+		elif joy.buttons[1] == 1: #B button
+			self.position = 2 #close 
 
 		self.send_info()
 
@@ -75,8 +75,8 @@ class ScienceController:
 	def send_info(self):
 
 		canFrame = self.send_flap()
-		canFrame2= self.send_spin()
-		canFrame3= self.send_limit()
+		canFrame2 = self.send_spin()
+		canFrame3 = self.send_limit()
 		
 		self.pub.publish(canFrame)
 		self.pub.publish(canFrame2)
